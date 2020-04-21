84f4dbe6584a09129766e0f86b59b9a123db4781
Unknown <nicholasdlchan60@gmail.com>
2020-04-21 17:20:54 -0400

    Add netlify config
    Also fixed the GraphQL requests

---
+++ src/components/SubmitForm.js
@@ -0,0 +1,87 @@
+import { Link } from "gatsby"
+import React, {Component} from "react"
+import {TextField, Button} from '@material-ui/core';
+
+import axios from "axios";
+class SubmitForm extends Component{
+  onSubmitForm = () =>{
+    console.log("test");
+  }
+  constructor(props){
+    super(props);
+
+    this.state ={
+      email: '',
+      pass: '',
+      url: ''
+    }
+  }
+
+  handleChange = (prop) => (event) => {
+    this.setState({ [prop]: event.target.value });
+  };
+  validPass = () => {
+    let email = this.state.email;
+    let url = this.state.url;
+    let pass = this.state.pass;
+    if(pass != GATSBY_PASS || url == "" || email == ""){
+      return false;
+    }else{S
+      return true;
+    }
+  }
+  submitButton = () => {
+    if(this.validPass() === false){
+    }else{
+      let email = this.state.email;
+      let url = this.state.url;
+      let body = {
+        query: `
+	mutation ($boardId: Int!, $groupId: String!, $itemName: String!, $columnValues: JSON!) {
+	  create_item (
+	    board_id: $boardId,
+	    group_id: $groupId,
+	    item_name: $itemName,
+	    column_values: $columnValues
+	  ) {
+	    id
+	  }
+	}
+  `,
+        variables: {
+          boardId: 428943437,
+          groupId: "topics",
+          itemName: "jsTest (ignore)",
+          columnValues: JSON.stringify({ "owner": email, "gsheetslink": url  })
+        }
+      }
+      let token = GATSBY_MONDAY;
+
+      axios.post(
+        "https://api.monday.com/v2/", body, {
+          headers: { Authorization: "Bearer " + token}
+        }
+      ).then((result) => {
+        console.log(result.data);
+      }).catch((err) => {
+        console.error(err);
+      })
+
+    }
+    }
+
+
+  render() {
+    return(
+      <p>
+        <TextField onChange={this.handleChange('email')} required id="email" label="Email" variant="outlined" />
+        <TextField onChange={this.handleChange('url')} required id="link" label="Sheets Link" variant="outlined" />
+        <TextField onChange={this.handleChange('pass')} required id="pass" label="Password" type="Password" variant="outlined" />
+        <p>{this.state.email + " " + this.state.url}</p>
+        <Button onClick={this.submitButton} variant="outlined">Bless</Button>
+      </p>
+    )
+  }
+}
+
+export default SubmitForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,18 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+import SubmitForm from "../components/SubmitForm"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Tired of Googling prospect information?</h1>
+    <p>Save time by sending a sheet with your leads for enrichment.</p>
+    <a href="https://www.google.ca" target="_blank">Make Sure You Use This Template</a>
+    <p>Fill in this form with your work email, link to your gSheets with leads, and the right password</p>
+    <SubmitForm />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
